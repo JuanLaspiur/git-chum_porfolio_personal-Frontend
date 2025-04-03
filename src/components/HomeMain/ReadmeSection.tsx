93c641391377eee085989b7a164900df1f86196a
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-04-03 17:01:00 -0300

    Guardar

--- src/components/HomeMain/ReadmeSection.tsx
+++ src/components/HomeMain/ReadmeSection.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-import translationsData from "./HomeMainTranslations.json"; 
+import translationsData from "./HomeMainTranslations.json";
 
 interface ReadmeSectionProps {
   language: "es" | "en";
 }
 
 const ReadmeSection = ({ language }: ReadmeSectionProps) => {
-  const [translations, setTranslations] = useState(translationsData[language] || translationsData["en"]);
+  const [translations, setTranslations] = useState(
+    translationsData[language] || translationsData["en"]
+  );
 
   useEffect(() => {
     setTranslations(translationsData[language] || translationsData["en"]);
@@ -14,9 +16,15 @@ const ReadmeSection = ({ language }: ReadmeSectionProps) => {
 
   return (
     <div className="info">
-       <div className="readme-header">JuanLaspiur / <span className="readme-file">README.md</span></div>
+      <div className="readme-header">
+        JuanLaspiur / <span className="readme-file">README.md</span>
+      </div>
       <h1>👋 {translations.name} - {translations.personalPage}</h1>
-      <a href="https://personal-webpage-frontend.vercel.app/" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://personal-webpage-frontend.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         https://personal-webpage-frontend.vercel.app/
       </a>
       <hr className="divider" />
@@ -26,16 +34,31 @@ const ReadmeSection = ({ language }: ReadmeSectionProps) => {
         <li>{translations.frameworks}</li>
         <li>{translations.databases}</li>
         <li>{translations.tools}</li>
+        <li>{translations.devOps}</li> {/* Agregamos DevOps */}
       </ul>
       <hr className="divider" />
       <h2>{translations.contact}</h2>
-      <p>{translations.email}: <a href="mailto:laspiurperezjuan@gmail.com">laspiurperezjuan@gmail.com</a></p>
-      <p>{translations.instagram}: <a href="https://instagram.com/jlaspiur" target="_blanck">@jlaspiur</a></p>
-      <p>{translations.linkedin}: <a href="https://www.linkedin.com/in/laspiurperezjuan/" target="_blanck">Juan Laspiur</a></p>
+      <p>
+        {translations.email}:{" "}
+        <a href="mailto:laspiurperezjuan@gmail.com">
+          laspiurperezjuan@gmail.com
+        </a>
+      </p>
+      <p>
+        {translations.instagram}:{" "}
+        <a href="https://instagram.com/jlaspiur" target="_blank">@jlaspiur</a>
+      </p>
+      <p>
+        {translations.linkedin}:{" "}
+        <a
+          href="https://www.linkedin.com/in/laspiurperezjuan/"
+          target="_blank"
+        >
+          Juan Laspiur
+        </a>
+      </p>
     </div>
   );
 };
 
 export default ReadmeSection;
-
-  

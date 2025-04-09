34d4a90251440a95378f7cc11aa94ab7a603d903
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-04-09 18:33:39 -0300

    Guardar

--- src/components/ProyectMain/ProjectGallery.tsx
+++ src/components/ProyectMain/ProjectGallery.tsx
@@ -86,6 +86,7 @@ const ProjectGallery: React.FC<ProjectGalleryProps> = ({ isDarkMode }) => {
             loop
             muted
             playsInline
+            preload="metadata"
           >
             Tu navegador no soporta el video HTML5.
           </video>
